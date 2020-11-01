cc2c150edcfeb82c3aec42bd371ea4ebdce328b4
nnaboon <srisawasdina@gmail.com>
2020-11-02 00:56:23 +0700

    s

--- src/pages/CustomerBarDetail.tsx
+++ src/pages/CustomerBarDetail.tsx
@@ -10,77 +10,9 @@ import CustomerBarPic from './CustomerBarPic';
 
 
 function CustomerBarDetail({match} : {match:any}) {
-    /*
-    const [ bardetail, setBardetail] = useState({});
-    useEffect(() => {
-      axios.get("https://docs.google.com/document/d/1utsUs99LkGp568qHs9GB9eOB3p4KtV_7rl0FF7TE7wc/edit?usp=sharing").then(result => {
-        const {data} = result;
-        console.log(data);
-        setBardata(data);
-      });
-    },[] );
-    */
-    // return (
-    //     <div className='bgg'>
-    //         <Navbar2 />
-    //         <div>
-    //             <br/><br/>
-    //             <header>
-    //                 <h1 className='nametext3'>Bar's Name</h1>
-    //             </header>
-    //             <br/>
-    //             <section>
-    //                 <div className="box0">
-    //                     <p className="boxtext">
-    //                         Bar<br/>
-    //                         Pic
-    //                     </p>
-    //                 </div>
-    //             </section>
-    //             <section>
-    //                 <div className="box1">
-    //                     <p className="boxtext">
-    //                         Bar<br/>
-    //                         Pic
-    //                     </p>
-    //                 </div>
-    //                 <div className="box2">
-    //                     <p className="boxtext">
-    //                         Bar<br/>
-    //                         Pic
-    //                     </p>
-    //                 </div>
-    //             </section>
-    //             <p className='destext'>
-    //                 Bar's Description: The great bar with the great views<br/><br/>
-    //                 Open-Time/Close-Time: 18.00 PM- 12.00 AM<br/><br/>
-    //                 Close On: Monday<br/><br/>
-    //                 Bar's Rule: If you want larger table, please tell us<br/> one day before your reserved date<br/><br/>
-    //                 LINE ID: Bar123456<br/>
-    //                 Tel: 012-345-6789<br/><br/>
-    //                 Address: 123 Soi Phaholyothin, Ladyao Subdistrict,<br/> Chatuchak District, Bangkok 10900<br/><br/>
-    //                 Bar's Rule: If you want larger table, please tell us<br/> one day before your reserved date<br/><br/>
-    //             </p>
-    //             <div className='cenbutton'>
-    //                 <CustomerBarForm/>
-    //             </div>
-    //             <br/><br/><br/>
-    //         </div>
-    //     </div>
-    // );
-
+    
     const barID = match.params.barID;
     const [images,setImages] = useState<any>('');
-    /*
-    const [ bardetail, setBardetail] = useState({});
-    useEffect(() => {
-      axios.get("https://docs.google.com/document/d/1utsUs99LkGp568qHs9GB9eOB3p4KtV_7rl0FF7TE7wc/edit?usp=sharing").then(result => {
-        const {data} = result;
-        console.log(data);
-        setBardata(data);
-      });
-    },[] );
-    */
 
    useEffect(() => {
     axios.get(`http://35.240.130.253:3001/bars/${barID}`, {
@@ -93,9 +25,9 @@ function CustomerBarDetail({match} : {match:any}) {
                 });      
     },[])
     return (
-        <div className="bgg">
+        <div>
             <Navbar2 />
-            <div>
+            <div className="bgg">
                 {/*<ul>{bardetail && bardetail.map(item => <li key={item._id}> {item._id} </li>)} </ul>*/}
                 <br/><br/>
                 <header>
